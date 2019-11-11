658bd19a5441103fa8fbb1c47a3922ded20e8e3d
michal <michal@secretescapes.com>
2019-11-11 17:10:26 +0100

    creation of fixversion via command line argument and possibility of providing own release notes to fill the tickets that not have them yet

--- src/main/groovy/com/secretescapes/jirereleasenotesauto/JiraReleaseNotes.groovy
+++ src/main/groovy/com/secretescapes/jirereleasenotesauto/JiraReleaseNotes.groovy
@@ -5,8 +5,21 @@ import groovy.json.JsonSlurper
 import groovyx.net.http.ContentType
 import groovyx.net.http.HTTPBuilder
 import groovyx.net.http.Method
+import groovyx.net.http.RESTClient
 
 class JiraReleaseNotes {
+	String jiraURL
+	String encoded64AuthInfo
+	RESTClient restClient
+
+	public JiraReleaseNotes(String jiraURL, String encoded64AuthInfo) {
+		this.jiraURL = jiraURL
+		this.encoded64AuthInfo = encoded64AuthInfo
+
+		restClient = new RESTClient(jiraURL)
+		restClient.headers['Authorization'] = 'Basic '+ encoded64AuthInfo
+		restClient.handler.failure = { resp -> println "ERROR: "+ resp.statusLine; return resp }
+	}
 
 	/**
 	 *
@@ -16,7 +29,7 @@ class JiraReleaseNotes {
 	 *
 	 * @return ReleaseNotes object containing JiraTickets related to the fixVersion
 	 */
-	public ReleaseNotes getFixNotes(String jiraURL, String encoded64AuthInfo, String fixVersion) {
+	public ReleaseNotes getFixNotes(String fixVersion) {
 		def http = new HTTPBuilder(jiraURL)
 		//http.ignoreSSLIssues() -> Only uncomment if having SSL issues
 
@@ -53,6 +66,75 @@ class JiraReleaseNotes {
 		}
 		releaseNotes
 	}
+
+
+
+	public void createFixVersionAndLinkWithTickets(String fixVersionToCreate, String issueIds) {
+		createFixVersion(fixVersionToCreate)
+
+		issueIds.split(",").each { issueId ->
+		println "/rest/api/2/issue/${issueId.trim()}"
+
+				def jsonObj =[	
+				"update": [
+					"fixVersions": [["add": 
+						  ["name": fixVersionToCreate]
+					]]
+				  ]
+				]
+			
+			
+			def response  = restClient.put(
+				path: "/rest/api/2/issue/${issueId.trim()}",
+				contentType: ContentType.JSON,
+				body: jsonObj)
+
+		}
+	}
+
+	
+	/**
+	 *
+	 */
+	 private createFixVersion(String fixVersionToCreate) {
+		def body =[	
+				"name": fixVersionToCreate,
+				"project": "DEV"
+			]
+		
+		def resp = restClient.post(
+			requestContentType: ContentType.JSON, 
+			body: body,
+			path: "/rest/api/2/version")
+		if(resp?.status?.toString()?.startsWith("2")) {
+			println "Version ${fixVersionToCreate} created. Response: ${resp?.statusLine}"
+		} else if (resp?.status?.toString()?.startsWith("4")) {
+			println "Version ${fixVersionToCreate} not created. Possibly exists already "
+		} else {
+			println "Version ${fixVersionToCreate} not created." + resp.properties
+		}
+	 }
+	/**
+	 * Updates JIRA issue with given ID with release notes provided as param
+	 */
+	 private updateReleaseNotes(String issueId, String releaseNotes) {
+		println "/rest/api/2/issue/${issueId.trim()}"
+
+			def jsonObj =[	
+			"fields": 
+				[
+					"customfield_10600": releaseNotes
+				]
+			]
+				
+		def response  = restClient.put(
+			path: "/rest/api/2/issue/${issueId.trim()}",
+			contentType: ContentType.JSON,
+			body: jsonObj)
+
+		println "Notes for ${issueId} added."
+		
+	 }
 
 	/**
 	 *
--- src/main/groovy/com/secretescapes/jirereleasenotesauto/MainApp.groovy
+++ src/main/groovy/com/secretescapes/jirereleasenotesauto/MainApp.groovy
@@ -13,14 +13,40 @@ class MainApp {
 	 * 1. Jira URL: e.g. http://myjira.atlassian.net
 	 * 2. Base 64 encoded username and password in the format of <username password> - more info @ https://developer.atlassian.com/jiradev/jira-apis/jira-rest-apis/jira-rest-api-tutorials/jira-rest-api-example-basic-authentication
 	 * 3. Release version number - e.g. 2.81
+	 * 4. (optional) list of ticket ids - e.g. DEV-3444,DEV-5421. Tickets will be attached to provided fixVersion
+	 * 5. (optional) Release notes that should be used as release notes for tickets attached to fixVersion prvided as 4th param
+
 	 */
 	public static void main(String[] args) {
-		if (args.size() != 3) {
-			throw new IllegalArgumentException("Must enter just three arguments - jira URL e.g. https://myjira.atlassian.net, Your encoded64 username and password, " +
-					"and the release version number.\n Full command line example: e.g. jar https://myjira.atlassian.net ZGVWltMQ= 2.71")
+		if ((args.size() != 3) && (args.size() != 5)) {
+			println "Number of arguments passed: " + args.size()
+			throw new IllegalArgumentException(
+			"Must enter three or four arguments \n" +
+			 "- jira URL e.g. https://myjira.atlassian.net, \n" +
+			 "- Your encoded64 username and password, " +
+			 "- the release version number - either existing one - if you release tickets already linked to fixVersion, or not existing one - it you want to create fixVersion and link it to tickets listed in next param \n" +
+			 "- (optional) list of ticket ids - e.g. DEV-3444,DEV-5421. Tickets will be attached to fixVersion provided in 3rd param \n"+
+			 "- (optional) the release note that should be used for tickets listed in 4th param\n" +
+			 " Full command line example: e.g. jar https://myjira.atlassian.net ZGVWltMQ= 2.71 " + 
+			 " or jar https://myjira.atlassian.net ZGVWltMQ= 2.12.1 DEV-45234,DEV-55444 'js improvements related to rendering speed'") 
 		} else {
-			JiraReleaseNotes jiraReleaseNotes = new JiraReleaseNotes()
-			ReleaseNotes releaseNotes = jiraReleaseNotes.getFixNotes(args[0], args[1], args[2])
+			JiraReleaseNotes jiraReleaseNotes = new JiraReleaseNotes(args[0], args[1])
+			if(args.size() == 5) {
+				System.out.println('five args')
+				jiraReleaseNotes.createFixVersionAndLinkWithTickets(args[2], args[3])
+				ReleaseNotes notYetFilledNotes = jiraReleaseNotes.getFixNotes(args[2])
+				notYetFilledNotes.releaseIssues.each { releaseIssue ->
+					System.out.println(releaseIssue.apiLink  + releaseIssue.note)
+					if(!releaseIssue.note) {
+						println "Release notes for ${releaseIssue.id} not found, will update them now"
+						jiraReleaseNotes.updateReleaseNotes(releaseIssue.id, args[4].trim())
+					} else {
+						println "Release notes for ${releaseIssue.id} already existed, no need to update"
+					}
+				}
+			}
+
+			ReleaseNotes releaseNotes = jiraReleaseNotes.getFixNotes(args[2])
 			String notesAsHTML = jiraReleaseNotes.formatAsHtml(releaseNotes)
 			File file = new File('.', 'release-notes.html')
 			file.write(notesAsHTML)
